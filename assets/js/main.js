0748f56bf03d5c352d873dfbe7f9e00f1cf334b0
supercodemereguenduez <supercode.emreguenduez@gmail.com>
2022-06-03 08:48:01 +0200

    rdy

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+// Lev1_1_js-vertiefung_math_math-pi
+// let x = Math.PI;
+
+// Lev1_2_js-vertiefung_math_math-round
+//  
+// let array = [
+//     3.14,
+//     193.4464,
+//     0.8596433607,
+//     -2.940629089,
+// ];
+
+// const runden = arr => arr.forEach(x => console.log(Math.round(x)));
+
+// runden(array);
+
+// Lev1_3_js - vertiefung_math_math - random_math - floor
+
+// document.getElementById("randomNum").innerHTML =
+//     Math.floor(Math.random() * 333);
+
+// document.getElementById("randomNum1_10").innerHTML =
+//     Math.floor(Math.random() * 11);
+
+// document.getElementById("randomNum1_100").innerHTML =
+//     Math.floor(Math.random() * 101);
+
+// Lev1_4_js-vertiefung_math_math-min_math-max
+
+// document.getElementById("min").innerHTML = Math.min(393, 205, 479, 47, 376, 267, 385, 2, 190, 383, 286, 462, 115, 138, 331, 409, 427, 245, 224, 276, 483, 55, 147, 177, 208, 347, 135, 282, 33, 270, 475, 426, 476, 453, 474, 351, 219, 358, 354, 410, 240, 139, 371, 356, 277, 202, 264, 204, 447, 488);
+
+// document.getElementById("max").innerHTML = Math.max(393, 205, 479, 47, 376, 267, 385, 2, 190, 383, 286, 462, 115, 138, 331, 409, 427, 245, 224, 276, 483, 55, 147, 177, 208, 347, 135, 282, 33, 270, 475, 426, 476, 453, 474, 351, 219, 358, 354, 410, 240, 139, 371, 356, 277, 202, 264, 204, 447, 488);
+
+// Lev2_1_js-vertiefung_math_math-round_toFixed_RETHINK
+
+// let num = 3.1415926535;
+// let n = num.toFixed(5);
+
+// document.getElementById("roundTo").innerHTML = n;
+
+// let gamne = () => {
+//     let schaetzen = prompt("Schätzen Sie die Nummer zwischen 1 und 10");
+//     let randomNum1_11 = Math.floor(Math.random() * 11);
+//     schaetzen == randomNum1_11 ? console.log('Richtig') : console.log(`Du hast verloren. Die Nummer lautet ${randomNum1_11}.`)
+// }
+
+// gamne();
